fb4b3b03ba8d72a78f707325a1f967b7d376d93b
FerasSalloum <ferassalloum1999l@gmail.com>
2025-10-28 09:50:14 +0300

    add pages and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Singin from "./pages/Singin";
+import Singup from "./pages/Singup";
 const App = () => {
-  return <div className="text-3xl text-red-500">app</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/projects" element={<Projects />}></Route>
+      <Route path="/singin" element={<Singin />}></Route>
+      <Route path="/singup" element={<Singup />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
