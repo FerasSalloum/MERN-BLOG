d795eab74383f8a6393edf583746faa904eaab1d
FerasSalloum <ferassalloum1999l@gmail.com>
2025-11-01 20:00:52 +0300

    create signin api route

--- api/routers/auth.route.js
+++ api/routers/auth.route.js
@@ -1,7 +1,9 @@
 import expres from "express"
-import { singnup } from "../controller/auth.controller.js";
+import { singnin, singnup } from "../controller/auth.controller.js";
 
 const router = expres.Router();
 
 router.post("/sign-up",singnup)
+router.post("/sign-in",singnin)
+
 export default router
